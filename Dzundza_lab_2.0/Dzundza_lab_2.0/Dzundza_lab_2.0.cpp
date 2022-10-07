de461dc8617b89bf1292e66896923669cf1ca413
pelmen166 <92636967+pelmen166@users.noreply.github.com>
2022-10-07 09:01:21 +0300

    Update Dzundza_lab_2.0.cpp

--- Dzundza_lab_2.0/Dzundza_lab_2.0/Dzundza_lab_2.0.cpp
+++ Dzundza_lab_2.0/Dzundza_lab_2.0/Dzundza_lab_2.0.cpp
@@ -1,4 +1,4 @@
-﻿// Lab_02.cpp
+// Lab_02.cpp
 // < Дзундза Дмитро >
 // Лабораторна робота № 2.
 // Лінійні програми.
@@ -7,14 +7,14 @@
 using namespace std;
 int main()
 {
-	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	double x;
+	double z1;
+	//double z2;
 
 	int alpha;
 	cout << "alpha = "; cin >> alpha;
 	z1 = cos(alpha)+cos(2*alpha)+cos(6*alpha)+cos(7*alpha);
-	z2 = 4*cos(alpha/2)*cos(5/2*alpha)*cos(4*alpha);
+	//z2 = 4*cos(alpha/2)*cos(5/2*alpha)*cos(4*alpha);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
